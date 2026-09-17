876510e75bd6fa28b7feb6f210a1545930a86b31
Stephensaz <jeff@stephensaz.com>
2026-09-17 10:27:43 -0700

    Restore report layout exports for M8-017

--- src/presentation/layout/__init__.py
+++ src/presentation/layout/__init__.py
@@ -1,6 +1,9 @@
+from .report_shell import ReportLayoutShell, ReportSectionSlot
 from .responsive import ResponsiveLayoutPlan, ResponsiveLayoutRegistry, ResponsiveLayoutRuntime, ResponsiveMode
 
 __all__ = [
+    "ReportLayoutShell",
+    "ReportSectionSlot",
     "ResponsiveLayoutPlan",
     "ResponsiveLayoutRegistry",
     "ResponsiveLayoutRuntime",
