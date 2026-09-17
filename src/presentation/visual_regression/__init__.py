eaf58e601f48cba6483b1ec9c3b1463751bc9500
Stephensaz <jeff@stephensaz.com>
2026-09-17 16:55:58 -0700

    Export M8-031 visual regression harness

---
+++ src/presentation/visual_regression/__init__.py
@@ -0,0 +1,15 @@
+from .capture import StructuralAssertion, VisualCapture, VisualCaptureHarness, VisualRegressionRegistry
+from .compare import VisualComparator, VisualComparison
+from .diff_classifier import VisualDiffClass, VisualDiffClassifier, VisualDiffDecision
+
+__all__ = [
+    "StructuralAssertion",
+    "VisualCapture",
+    "VisualCaptureHarness",
+    "VisualRegressionRegistry",
+    "VisualComparator",
+    "VisualComparison",
+    "VisualDiffClass",
+    "VisualDiffClassifier",
+    "VisualDiffDecision",
+]
