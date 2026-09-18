60445e73fb5cc1b05c79c88912dc2ced450c4778
Stephensaz <jeff@stephensaz.com>
2026-09-17 17:14:09 -0700

    Restore frozen M8-033 certification artifact

---
+++ src/presentation/certification/__init__.py
@@ -0,0 +1,3 @@
+from .m8_033 import CertificationBundle, EvidenceRecord, M8033CertificationRunner, write_bundle
+
+__all__ = ["CertificationBundle", "EvidenceRecord", "M8033CertificationRunner", "write_bundle"]
